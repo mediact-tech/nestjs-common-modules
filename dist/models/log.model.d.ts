b9d2d5cea74d68c8b9818e4bb0ec59bb22fdaa8e
ThuT <thut@ThuTs-MacBook-Air-2.local>
2025-12-20 22:39:11 +0700

    first commit

---
+++ dist/models/log.model.d.ts
@@ -0,0 +1,16 @@
+export declare class LogModel {
+    correlationId?: string;
+    method?: string;
+    endpoint?: string;
+    userId?: string;
+    body?: string;
+    param?: string;
+    message?: string;
+    response?: string;
+    errorStack?: string;
+    statusCode?: string;
+    httpStatusCode?: string;
+    header?: string;
+    toReadAbleFormat(): string;
+}
+//# sourceMappingURL=log.model.d.ts.map
